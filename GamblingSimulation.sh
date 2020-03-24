19e641167cf26dec6a8446333d55de9d0397df15
root <root@localhost.localdomain>
2020-03-24 15:06:56 +0000

    First Commit

--- GamblingSimulation.sh
+++ GamblingSimulation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 
 #Use case 1
-day=1
+day=0
 start=100
 bet=1
 
@@ -19,7 +19,26 @@ fi
 #Use case 3
 high=$(($start + $(($start / 2))))
 low=$(($start - $(($start / 2))))
+day(){
+start=100
 while [ $start -lt $high ] && [ $start -gt $low ]
 do
 	bet
 done
+}
+
+#Use case 4
+while [ $day -lt 20 ]
+do
+	day
+	echo $start
+	if [ $start -eq 150 ]
+	then 
+		won=$(($won + 50))
+	else
+		lost=$(($lost + 50))
+	fi
+	day=$(($day + 1))
+done
+echo "Total won amount is $won"
+echo "Total lost amount is $lost"
